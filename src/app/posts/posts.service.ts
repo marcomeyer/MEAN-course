2b85a2db56f24aa5d10a1a99929df8f4119a2230
Marco Meyer <marco.meyer@finnova.com>
2020-08-15 13:30:55 +0200

    3.42 POSTing data from Ng

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,7 +2,6 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { stringify } from 'querystring';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -29,8 +28,13 @@ export class PostsService {
 
     addPost(title: string, content: string) {
         const post: Post = {id: null, title, content};
+        this.http
+            .post<{message: string}>('http://localhost:3000/api/posts', post)
+            .subscribe((responseData) => {
+                console.log(responseData.message);
 
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
+                this.posts.push(post);
+                this.postsUpdated.next([...this.posts]);
+            });
     }
 }
