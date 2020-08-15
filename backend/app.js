9a4d0e5b51aae82c2c78abf4f61a0f06f5b6a5e0
Marco Meyer <marco.meyer@finnova.com>
2020-08-15 12:41:09 +0200

    3.38 fetching serverside posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first');
-    next();
+app.use('/api/posts',(req, res) => {
+    const posts = [
+        { id: 'a408thgg', title: 'First server-side post', content: 'This is coming from the server'},
+        { id: 'z3uhgge0', title: 'Second server-side post', content: 'This is coming from the server!'}
+    ];
+
+    res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts});
 });
 
-app.use((req, res, next) => {
-    res.send('Hello');
+app.use('/',(req, res) => {
+    res.send('<a href="/api/posts">GO</a>');
 });
 
 module.exports = app;
-
