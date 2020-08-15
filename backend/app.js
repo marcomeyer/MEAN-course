fc650434babbb29fa209cf645bc59651c9a711a4
Marco Meyer <marco.meyer@finnova.com>
2020-08-15 13:15:45 +0200

    3.40 add headers (CORS,...)

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/api/posts',(req, res) => {
     const posts = [
         { id: 'a408thgg', title: 'First server-side post', content: 'This is coming from the server'},
