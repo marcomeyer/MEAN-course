386b37e54fd540d7e28c91dcc8375c0f3d941a4f
Marco Meyer <marco.meyer@finnova.com>
2020-08-16 09:36:15 +0200

    4.52 create POST instance

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const Post = require('./models/post');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -13,7 +14,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts',(req, res) => {
-    const post = req.body;
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
     console.log(post);
     res.status(201).json(
         {
