52104502f378178e445a437ce532b01c442e974c
Marco Meyer <marco.meyer@finnova.com>
2020-08-16 10:05:47 +0200

    4.56 transform data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
@@ -13,13 +14,21 @@ export class PostsService {
     ) {}
 
     getPosts() {
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-            .subscribe(
-                (postData) => {
-                    this.posts = postData.posts;
-                    this.postsUpdated.next([...this.posts]);
-                }
-        );
+      this.http
+        .get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+        .pipe(map(postData => {
+          return postData.posts.map(post => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id
+            };
+          });
+        }))
+        .subscribe((tranformedPosts) => {
+          this.posts = tranformedPosts;
+          this.postsUpdated.next([...this.posts]);
+        });
     }
 
     getPostUpdateListener(){
