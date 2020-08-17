2e1b4df632aca4b0628a53720c102b097ab28d87
Marco Meyer <marco.meyer@finnova.com>
2020-08-17 22:19:14 +0200

    5.66 create the edit form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
@@ -12,12 +13,21 @@ import { PostsService } from '../posts.service';
 export class PostCreateComponent implements OnInit{
   enteredTitle = '';
   enteredContent = '';
+  private mode = 'create';
+  private postId: string;
+  private post: Post;
 
   constructor(
-    private postsService: PostsService
+    private postsService: PostsService,
+    public route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      this.postId = paramMap.get('postId');
+      this.mode = (this.postId == null) ? 'create' : 'edit';
+      this.post = (this.postId == null) ? null : this.postsService.getPost(this.postId);
+    });
   }
 
   onAddPost(form: NgForm)
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -38,6 +38,10 @@ export class PostsService {
         return this.postsUpdated.asObservable();
     }
 
+    getPost(id: string) {
+      return {...this.posts.find(p => p.id === id)};
+    }
+
     addPost(title: string, content: string) {
         const post: Post = {id: null, title, content};
         this.http
