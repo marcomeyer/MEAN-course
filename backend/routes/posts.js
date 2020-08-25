567e3b1097167d7c987dbfa73e064a285d95ed54
Marco Meyer <marco.meyer@finnova.com>
2020-08-25 15:18:18 +0200

    tidy up

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const Post = require('../models/post');
 
+
+router.get('',(req, res) => {
+  Post
+    .find()
+    .then(docs => {
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: docs});
+    });
+});
+
 router.post('',(req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -20,6 +31,17 @@ router.post('',(req, res) => {
   });
 });
 
+router.get('/:id',(req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if(post){
+      res.status(200).json(post);
+    }
+    else {
+      res.status(404).json({message: 'Post not found!'});
+    }
+  })
+});
+
 router.put('/:id', (req, res, next) => {
   const post = new Post({
     _id: req.body.id,
@@ -32,27 +54,6 @@ router.put('/:id', (req, res, next) => {
     res.status(200).json({message: 'Update successful!'});
   });
 
-});
-
-router.get('',(req, res) => {
-  Post
-    .find()
-    .then(docs => {
-      res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: docs});
-    });
-});
-
-router.get('/:id',(req, res, next) => {
-Post.findById(req.params.id).then(post => {
-  if(post){
-    res.status(200).json(post);
-  }
-  else {
-    res.status(404).json({message: 'Post not found!'});
-  }
-})
 });
 
 router.delete('/:id', (req, res, next) => {
