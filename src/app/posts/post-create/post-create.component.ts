ae745063edf41083c496f1433cbced703bc0a084
Marco Meyer <marco.meyer@finnova.com>
2020-08-25 17:28:26 +0200

    6.78 finish the mime type validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { Post } from '../post.model';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -27,7 +28,9 @@ export class PostCreateComponent implements OnInit{
     this.form = new FormGroup({
       title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
       content: new FormControl(null, {validators: [Validators.required]}),
-      image: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]}),
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
