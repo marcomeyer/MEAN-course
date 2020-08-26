d61270553dbdb9673d8c8cf5de99e1a0f4464acf
Marco Meyer <marco.meyer@finnova.com>
2020-08-26 07:49:41 +0200

    6.79 add serverside for image saving

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
 
 const Post = require('../models/post');
 
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jng',
+  'image/jpg': 'jng',
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+
+    if(isValid) {
+      error = null;
+    }
+
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
 
 router.get('',(req, res) => {
   Post
@@ -14,7 +38,7 @@ router.get('',(req, res) => {
     });
 });
 
-router.post('',(req, res) => {
+router.post('', multer(storage).single('image'), (req, res) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
