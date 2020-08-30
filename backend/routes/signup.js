1c28c393eac936899a6b5a4d426c48bd5b6317cc
Marco Meyer <marco.meyer@finnova.com>
2020-08-30 21:35:28 +0200

    8.103 sending token to the frontend

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-const user = require("../models/user");
 
 const router = express.Router();
 
@@ -31,6 +30,7 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
+  let fetchedUser;
   User
     .findOne({ email:req.body.email })
     .then(user => {
@@ -39,7 +39,7 @@ router.post("/login", (req, res, next) => {
           message: "Auth failed"
         });
       }
-
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
@@ -50,14 +50,19 @@ router.post("/login", (req, res, next) => {
       }
 
       const token = jwt.sign(
-        { email: user.email, userId: user._id },
+        { email: fetchedUser.email, userId: fetchedUser._id },
         'secret_this_should_be_longer',
         { expiresIn: '1h' }
       );
+
+      res.status(200).json({
+        token
+      });
     })
     .catch(error => {
+      console.log(error);
       res.status(401).json({
-        message: "Auth failed"
+        message: "Auth failed with error"
       });
     });
 });
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { AuthData } from './auth-data-model';
 @Injectable({ providedIn: 'root'})
 export class AuthService {
 
-  private url = 'http://localhost:3000/api/user/signup';
+  private url = 'http://localhost:3000/api/user';
 
   constructor(private http: HttpClient) { }
 
@@ -13,9 +13,19 @@ export class AuthService {
     const authData: AuthData = { email, password };
 
     this.http
-      .post(this.url, authData)
+      .post(this.url + '/signup', authData)
       .subscribe((response) => {
-        
+       console.log(response);
+    });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = { email, password };
+
+    this.http
+      .post(this.url + '/login', authData)
+      .subscribe((response) => {
+       console.log(response);
     });
   }
 }
