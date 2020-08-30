32241f1bf62a927feab1e43059b64f9f107d5e57
Marco Meyer <marco.meyer@finnova.com>
2020-08-30 21:50:49 +0200

    8.104 add middleware to protect routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -56,27 +57,31 @@ router.get('',(req, res) => {
     });
 });
 
-router.post('', multer({storage: storage}).single('image'), (req, res) => {
-  const url = req.protocol + '://' + req.get('host');
+router.post(
+  '',
+  checkAuth,
+  multer({storage: storage}).single('image'),
+  (req, res) => {
+    const url = req.protocol + '://' + req.get('host');
 
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url+"/images/" + req.file.filename
-  });
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url+"/images/" + req.file.filename
+    });
 
-  post.save().then(createdPost => {
+    post.save().then(createdPost => {
 
-    res.status(201).json(
-      {
-          message: 'Post added successfully',
-          post: {
-            ...createdPost,
-            id: createdPost._id
-          }
-      }
-    );
-  });
+      res.status(201).json(
+        {
+            message: 'Post added successfully',
+            post: {
+              ...createdPost,
+              id: createdPost._id
+            }
+        }
+      );
+    });
 });
 
 router.get('/:id',(req, res, next) => {
@@ -90,34 +95,40 @@ router.get('/:id',(req, res, next) => {
   })
 });
 
-router.put('/:id', multer({storage: storage}).single('image'), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-  if(req.file) {
-    const url = req.protocol + '://' + req.get('host');
-    imagePath = url + "/images/" + req.file.filename;
-  }
+router.put(
+  '/:id',
+  checkAuth,
+  multer({storage: storage}).single('image'),
+  (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if(req.file) {
+      const url = req.protocol + '://' + req.get('host');
+      imagePath = url + "/images/" + req.file.filename;
+    }
 
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath
-  });
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath
+    });
 
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: 'Update successful!'});
-  });
-
+    Post.updateOne({ _id: req.params.id }, post).then(result => {
+      console.log(result);
+      res.status(200).json({message: 'Update successful!'});
+    });
 });
 
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id;
-  console.log(id);
-  Post.deleteOne({ _id: id}).then(result => {
-    console.log(result);
-    res.status(200).json({message: 'Post deleted'})
-  });
+router.delete(
+  '/:id',
+  checkAuth,
+  (req, res, next) => {
+    const id = req.params.id;
+    console.log(id);
+    Post.deleteOne({ _id: id}).then(result => {
+      console.log(result);
+      res.status(200).json({message: 'Post deleted'})
+    });
 });
 
 module.exports = router;
