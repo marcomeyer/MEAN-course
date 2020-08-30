c8c61008c583e275ac53aa00144549154979a3d7
Marco Meyer <marco.meyer@finnova.com>
2020-08-30 22:46:57 +0200

    8.108 connect logout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -26,4 +26,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
