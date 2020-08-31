abb6d453d79d65c492212d81250c64e9656e6ac0
Marco Meyer <marco.meyer@finnova.com>
2020-08-31 21:14:47 +0200

    9.116 add user ID to posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -61,13 +61,14 @@ router.post(
   '',
   checkAuth,
   multer({storage: storage}).single('image'),
-  (req, res) => {
+  (req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
 
     const post = new Post({
       title: req.body.title,
       content: req.body.content,
-      imagePath: url+"/images/" + req.file.filename
+      imagePath: url+"/images/" + req.file.filename,
+      creator: req.userData.userId
     });
 
     post.save().then(createdPost => {
